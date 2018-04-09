5e8d09bc0bed49a0da704c41a89e4320c4e6bdcb
gjdame <gregoryjdame@gmail.com>
2018-04-09 13:42:41 -0700

    NULL check #10

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -10,6 +10,9 @@ int check_cycle(listint_t *list)
 	listint_t *fast = list;
 	listint_t *slow = list;
 
+	if (list == NULL)
+		return (0);
+
 	while (fast != NULL && slow != NULL)
 	{
 		fast = fast->next->next;
