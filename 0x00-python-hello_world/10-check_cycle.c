a4caf4c3e607014125036ea86b6797b27b778f1a
gjdame <gregoryjdame@gmail.com>
2018-04-09 13:23:12 -0700

    fix 3 & 4 to account for variable number

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,7 +1,15 @@
 #include "lists.h"
-
+/**
+ * check_cycle - checks for a loop in a linked list
+ * @list - pointer to a linked list
+ *
+ * Return: 1 if loop exists, 0 if not
+ */
 int check_cycle(listint_t *list)
 {
+	if (list == NULL)
+		return (0);
+	
 	listint_t *fast = list;
 	listint_t *slow = list;
 
