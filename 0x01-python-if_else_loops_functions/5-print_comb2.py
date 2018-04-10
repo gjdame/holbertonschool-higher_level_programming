4f213e25d96efce1f4a7a2f9c78527bd1a259076
gjdame <gregoryjdame@gmail.com>
2018-04-10 12:36:13 -0700

    pep8 #5

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(100):
     if i != 99:
-        print (format('%02d' % i), end=", ")
+        print("{}".format('%02d' % i), end=", ")
     else:
-        print(i)
+        print("{}".format(i))
