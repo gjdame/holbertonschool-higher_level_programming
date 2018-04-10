cb3482618ee0d600cc4a502ad629b4c4586a6ece
gjdame <gregoryjdame@gmail.com>
2018-04-10 15:05:34 -0700

    add 13 and list header

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -16,7 +16,7 @@ listint_t *insert_node(listint_t **head, int number)
 		return (NULL);
 	new->n = number;
 
-	if ((*head)->n > number || (*head) == NULL)
+	if (*head == NULL || (*head)->n > number)
 	{
 		new->next = *head;
 		*head = new;
