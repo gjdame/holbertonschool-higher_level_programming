15e707e703826e9c729f32d5026969b59842c1c3
gjdame <gregoryjdame@gmail.com>
2018-04-11 09:24:10 -0700

    redo of #6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-for i in range(0, 9):
-    for j in range(i + 1, 10):
-        print("{:d}{:d}".format(i, j), end="")
-        if i != 8 or j != 9:
-            print(end=", ")
+for i in range(0, 90):
+    tens = i / 10
+    mod = i % 10
+
+    if mod > tens:
+        if i != 89:
+            print("{}".format('%02d' % i), end=", ")
         else:
-            print("")
+            print("{}".format('%02d' % i))
