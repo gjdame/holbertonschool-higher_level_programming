52086e200f920c93487633ba40cb1127318c1224
gjdame <gregoryjdame@gmail.com>
2018-04-12 13:49:17 -0700

    initial commit #100

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+import sys
+from calculator_1 import add, sub, mul, div
+
+
+def main(argv):
+
+    result = 0
+
+    if len(argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+
+    a = int(argv[1])
+    b = int(argv[3])
+
+    if argv[2] == '+':
+        result = add(a, b)
+    elif argv[2] == '-':
+        result = sub(a, b)
+    elif argv[2] == '*':
+        result = mul(a, b)
+    elif argv[2] == '/':
+        result = div(a, b)
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+    print(result)
+if __name__ == "__main__":
+    import sys
+    main(sys.argv)
