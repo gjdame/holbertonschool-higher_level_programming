b7615ed7365cd8e6d76563782ae109a0db6df3c6
gjdame <gregoryjdame@gmail.com>
2018-04-12 15:21:23 -0700

    102-magic_calculation.py

---
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import magic_calculation_102
+
+
+def magic_calculation(a, b):
+    if a < b:
+        c = add(a, b)
+        for i in range(4,6):
+            c = add(c, i)
+            return(c)
+    else:
+        return (sub(a, b))
