ec2f7a8067a7dae82539a523b6e9107d8d12230b
gjdame <gregoryjdame@gmail.com>
2018-04-17 13:34:26 -0700

    pep8

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 
+
 def new_in_list(my_list, idx, element):
 
     if idx < 0 or idx >= len(my_list):
-        return(None)
+        new_list = my_list.copy()
+        return(new_list)
     new_list = my_list.copy()
     new_list.insert(idx, element)
     del new_list[idx + 1]
