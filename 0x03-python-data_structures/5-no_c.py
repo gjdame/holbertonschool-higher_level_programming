44b44e6cff222d1622bda298b6e6e0b351513574
gjdame <gregoryjdame@gmail.com>
2018-04-17 13:38:12 -0700

    pep8 and string format

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+
 def no_c(my_string):
    my_string = [i for i in my_string if i.upper() != 'C']
    my_string = ''.join(my_string)
--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 
+
 def print_matrix_integer(matrix=[[]]):
 
-   for row in matrix:
-      for elem in row:
-         print(elem, end='')
-         if elem is not row[len(row) - 1]:
-            print(end=' ')
-      print()
+    for row in matrix:
+        for elem in row:
+            print("{:d}".format(elem), end='')
+            if elem is not row[len(row) - 1]:
+                print(end=' ')
+        print()
