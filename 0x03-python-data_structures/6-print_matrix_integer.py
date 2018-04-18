b90ac83c37e7dd7981fe55d821da60356ea54a8f
gjdame <gregoryjdame@gmail.com>
2018-04-18 09:33:13 -0700

    pep8 style #6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,4 +4,4 @@
 def print_matrix_integer(matrix=[[]]):
 
     for row in matrix:
-        print (' '.join(["{:d}".format(elem) for elem in row]))
+        print(' '.join(["{:d}".format(elem) for elem in row]))
