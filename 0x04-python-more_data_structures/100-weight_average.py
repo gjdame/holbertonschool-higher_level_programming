7a02ca8ddf7163009c3bc44fb126122f955fbb26
gjdame <gregoryjdame@gmail.com>
2018-04-20 09:40:55 -0700

    initial commit 100-weight average

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+
+
+def weight_average(my_list=[]):
+    if my_list:
+        res = sum(a * b for a, b in my_list) / sum(b for a, b in my_list)
+        return (res)
+
+    return(0)
