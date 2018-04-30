3b2c070ca6d7bf7a8f9a65278a59de79f1fb4356
gjdame <gregoryjdame@gmail.com>
2018-04-30 13:35:54 -0700

    initial add 100

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+def safe_print_integer_err(value):
+
+    import sys
+
+    try:
+        print("{:d}".format(value))
+        return(True)
+    except ValueError as err:
+        sys.stderr.write("Exception: {}\n".format(err))
+        return(False)
