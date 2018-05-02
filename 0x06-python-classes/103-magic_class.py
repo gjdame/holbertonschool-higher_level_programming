90b43806722b6dde084c5749a5d578e365f45159
gjdame <gregoryjdame@gmail.com>
2018-05-01 19:17:13 -0700

    add import math and radius=0 in init

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,19 +1,23 @@
 #!/usr/bin/python3
+import math
 
 
 class MagicClass:
+    """Magic class that does same as given bytecode output"""
 
-    def __init__(self, radius):
-        self._MagicClass__radius = 0
+    def __init__(self, radius=0):
+        """initialize radius"""
+        self.__radius = 0
         if type(radius) is not int and type(radius) is not float:
             raise TypeError('radius must be a number')
-        else:
-            self._MagicClass__radius = radius
+        self.__radius = radius
 
     def area(self, radius):
+        """returns area"""
         return (self._MagicClass__radius ** 2 * math.pi)
 
     def circumference(self, radius):
+        """returns circumference"""
         return (2 * math.pi * self._MagicClass__radius)
 
 if __name__ == "__main__":
