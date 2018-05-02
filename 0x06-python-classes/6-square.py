17dbef33be1df65bad67ae3d04be5b6fdd9e1e2e
gjdame <gregoryjdame@gmail.com>
2018-05-01 18:07:06 -0700

    add tuple check

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -23,8 +23,7 @@ class Square:
 
     def area(self):
         """Determines area"""
-        res = self.__size ** 2
-        return(res)
+        return(self.__size ** 2)
 
     @property
     def size(self):
@@ -53,6 +52,8 @@ class Square:
             value: value of position
         """
         if type(value[0]) is not int or type(value[1]) is not int:
+            raise TypeError('position must be a tuple of 2 positive integers')
+        elif type(value) is not tuple:
             raise TypeError('position must be a tuple of 2 positive integers')
         elif value[0] < 0 or value[1] < 0:
             raise ValueError('position must be a tuple of 2 positive integers')
