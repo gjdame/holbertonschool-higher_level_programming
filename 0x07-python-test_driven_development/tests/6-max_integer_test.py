983068b664d7ad00aa3d5357a8284f59dc375a62
gjdame <gregoryjdame@gmail.com>
2018-05-03 11:15:03 -0700

    add initial tests for max int

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -5,4 +5,30 @@ import unittest
 max_integer = __import__('6-max_integer').max_integer
 
 class TestMaxInteger(unittest.TestCase):
-    
+
+    def testForImport(self):
+        self.assertTrue(__import__('6-max_integer').max_integer)
+
+    def testBasic(self):
+        my_list = [1, 3, 2, 4]
+        self.assertEqual(max_integer(my_list), 4)
+
+    def testBasic2(self):
+        A = 52
+        my_list = [1, 2, 4, A]
+        self.assertEqual(max_integer(my_list), A)
+
+    def testOneElement(self):
+        my_list = [1]
+        self.assertEqual(max_integer(my_list), 1)
+
+    def testEmpty(self):
+        my_list = []
+        self.assertEqual(max_integer(my_list), None)
+
+    def testFailBasic(self):
+        my_list = [1, 4, 'A']
+        self.assertRaises(TypeError, max_integer, my_list)
+
+if __name__ == '__main__':
+    unittest.main()
