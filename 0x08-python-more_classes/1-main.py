418f24847ef3c43a5ed2efae9aea5831a88b154a
gjdame <gregoryjdame@gmail.com>
2018-05-22 17:12:27 +0000

    gitignore now functional

--- 0x08-python-more_classes/1-main.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-Rectangle = __import__('1-rectangle').Rectangle
-
-my_rectangle = Rectangle(2, 4)
-print(my_rectangle.__dict__)
-
-my_rectangle.width = 4
-my_rectangle.height = 9
-print(my_rectangle.__dict__)
