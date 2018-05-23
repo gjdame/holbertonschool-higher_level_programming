105bf03f4fd16b8a00f335601de571e621a5c747
gjdame <gregoryjdame@gmail.com>
2018-05-23 01:20:29 +0000

    add advanced #1

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,9 +1,10 @@
-#!/usr/bin/python3
+ #!/usr/bin/python3
 
 '''
 Defines the class Base
 '''
 import json
+import csv
 
 
 class Base:
@@ -84,3 +85,45 @@ class Base:
             return([cls.create(**x) for x in my_list])
         except FileNotFoundError:
             return([])
+
+    @classmethod
+    def save_to_file_csv(cls, list_objs):
+        """saves to csv file
+        Args
+           cls - class
+           list_objs - list objects
+        """
+        if cls.__name__ is "Rectangle":
+            names = ['width', 'height', 'x', 'y', 'id']
+        else:
+            names = ['size', 'x', 'y', 'id']
+
+        filename = '{}.csv'.format(cls.__name__)
+
+        with open(filename, 'w', newline='') as f:
+            if list_objs:
+                writer = csv.DictWriter(f, fieldnames=names)
+                writer.writeheader()
+                for x in list_objs:
+                    writer.writerow(x.to_dictionary())
+            else:
+                writer.writerow([[]])
+
+    @classmethod
+    def load_from_file_csv(cls):
+        """returns a list of instances of cls
+        Args
+           cls - class
+        """
+        my_list = []
+        filename = '{}.csv'.format(cls.__name__)
+        try:
+            with open(filename, 'r') as f:
+                reader = csv.DictReader(f)
+                for row in reader:
+                    for key, val in row.items():
+                        row[key] = int(val)
+                    my_list.append(row)
+            return([cls.create(**x) for x in my_list])
+        except FileNotFoundError:
+            return([[]])
