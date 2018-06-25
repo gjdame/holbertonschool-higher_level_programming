e40c6a81a235f61e08ba3566f29ffae92dc02426
gjdame <gregoryjdame@gmail.com>
2018-06-25 21:07:09 +0000

    rework #9

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,8 +1,10 @@
  -- displays number of records with id of 89
-CREATE table IF NOT EXISTS second_table(`id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+CREATE table IF NOT EXISTS second_table(`id` INT,
 `name` VARCHAR(256),
-`score` INT);
-INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (NULL, 'John', 10);
-INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (NULL, 'Alex', 3);
-INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (NULL, 'Bob', 14);
-INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (NULL, 'George', 8);
+`score` INT)
+INSERT INTO `second_table` (`id`, `name`, `score`)
+VALUES
+       (1, 'John', 10),
+       (2, 'Alex', 3),
+       (3, 'Bob', 14),
+       (4, 'George', 8);
