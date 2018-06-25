62bd4cc12daeea57f485da1ac40929a475edca59
gjdame <gregoryjdame@gmail.com>
2018-06-25 21:11:11 +0000

    change comments for #9

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,4 +1,4 @@
- -- creates table second_table in the database hbtn_0c_0
+-- creates table second_table in the database hbtn_0c_0
 CREATE table IF NOT EXISTS second_table (`id` INT,
 `name` VARCHAR(256),
 `score` INT);
