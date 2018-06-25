3cdcb27bef81e64ca03cce6be58f83af63f48bd7
gjdame <gregoryjdame@gmail.com>
2018-06-25 21:13:49 +0000

    capitalize FROM in 13 & 14

--- 0x0D-SQL_introduction/13-change_class.sql
+++ 0x0D-SQL_introduction/13-change_class.sql
@@ -1,3 +1,3 @@
 -- removes all scores below 5 from second_table
-DELETE from second_table
+DELETE FROM second_table
 WHERE score <= 5;
--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -1,2 +1,2 @@
 -- computes average score of all records in second table
-SELECT AVG(score) AS average from second_table;
+SELECT AVG(score) AS average FROM second_table;
