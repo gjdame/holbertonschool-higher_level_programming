93bc1c524a4968ddc456a8307f02ff95245c197a
gjdame <gregoryjdame@gmail.com>
2018-06-26 21:38:30 +0000

    change column names for 13

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,5 +1,5 @@
 -- lists all shows in hbtn_0d_tvsows that have at least 1 genre linked
-SELECT tv_genres.name, COUNT(tv_show_genres.genre_id) AS number_shows
+SELECT tv_genres.name AS genre, COUNT(tv_show_genres.genre_id) AS number_shows
        FROM tv_show_genres
        INNER JOIN tv_genres
        ON tv_genres.id = tv_show_genres.genre_id
