f13412816893ae9587665d1ff1a7b8e51da6b95e
gjdame <gregoryjdame@gmail.com>
2018-07-20 01:54:53 +0000

    add 1 update to account for printing

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -10,18 +10,20 @@ def main(argv):
         print("Must enter 3 arguments")
         return
 
-    db = MySQLdb.connect (host="localhost",
+    db = MySQLdb.connect(host="localhost",
                          user=argv[1],
                          passwd=argv[2],
-                          db=argv[3],
-                          port=3306)
+                         db=argv[3],
+                         port=3306)
     cur = db.cursor()
     cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
 
     for row in cur.fetchall():
-        print (row)
+        if row[1][0] == 'N':
+            print(row)
 
     db.close()
+
 
 if __name__ == "__main__":
     import sys
