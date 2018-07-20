b496fff3bd47b7387956fa3af8a8a908a3115554
gjdame <gregoryjdame@gmail.com>
2018-07-20 03:08:04 +0000

    pycodestyle 3 & 4

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -10,18 +10,19 @@ def main(argv):
         print("Must enter 4 arguments")
         return
 
-    db = MySQLdb.connect (host="localhost",
+    db = MySQLdb.connect(host="localhost",
                          user=argv[1],
                          passwd=argv[2],
-                          db=argv[3],
-                          port=3306)
+                         db=argv[3],
+                         port=3306)
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name=%s" , [argv[4]])
+    cur.execute("SELECT * FROM states WHERE name=%s", [argv[4]])
 
     for row in cur.fetchall():
-        print (row)
+        print(row)
 
     db.close()
+
 
 if __name__ == "__main__":
     import sys
--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -10,18 +10,21 @@ def main(argv):
         print("Must enter 4 arguments")
         return
 
-    db = MySQLdb.connect (host="localhost",
+    db = MySQLdb.connect(host="localhost",
                          user=argv[1],
                          passwd=argv[2],
-                          db=argv[3],
-                          port=3306)
+                         db=argv[3],
+                         port=3306)
     cur = db.cursor()
-    cur.execute("SELECT cities.id, cities.name, states.name FROM cities INNER JOIN states ON states.id = cities.state_id ORDER BY cities.id ASC")
+    cur.execute("SELECT cities.id, cities.name, states.name\
+    FROM cities \
+    INNER JOIN states ON states.id = cities.state_id ORDER BY cities.id ASC")
 
     for row in cur.fetchall():
-        print (row)
+        print(row)
 
     db.close()
+
 
 if __name__ == "__main__":
     import sys
