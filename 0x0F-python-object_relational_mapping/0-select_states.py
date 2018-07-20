6695bf5a37e12bf3563dea70dd0f62ac12b19816
gjdame <gregoryjdame@gmail.com>
2018-07-20 03:29:06 +0000

    add #0 for pycodestyle

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -10,19 +10,21 @@ def main(argv):
         print("Must enter 3 arguments")
         return
 
-    db = MySQLdb.connect (host="localhost",
+    db = MySQLdb.connect(host="localhost",
                          user=argv[1],
                          passwd=argv[2],
-                          db=argv[3],
-                          port=3306)
+                         db=argv[3],
+                         port=3306)
     cur = db.cursor()
     cur.execute("SELECT * FROM states ORDER BY id ASC")
 
     for row in cur.fetchall():
-        print (row)
+        print(row)
 
     db.close()
 
+
 if __name__ == "__main__":
+
     import sys
     main(sys.argv)
