ba2d34e67f52fa17869aea688fa7b7b4b71a2869
gjdame <gregoryjdame@gmail.com>
2018-07-20 03:25:27 +0000

    add not found statement to #10

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -8,6 +8,7 @@ from sqlalchemy.orm import sessionmaker
 
 def main(argv):
 
+    flag = 0
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
                            .format(argv[1],
                                    argv[2], argv[3]), pool_pre_ping=True)
@@ -18,8 +19,11 @@ def main(argv):
     for state in session.query(State).filter(
             State.name == argv[4]).order_by(
             State.id).all():
-
-        print("{}".format(state.id))
+        if state:
+            flag = 1
+            print("{}".format(state.id))
+    if flag == 0:
+        print("Not found")
     session.close()
 
 
