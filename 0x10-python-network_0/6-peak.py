920bb011d9f6a0cdae8765994fca936ba6a07c97
gjdame <gregoryjdame@gmail.com>
2018-07-31 20:35:57 +0000

    add #6 pycode

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 '''find peak of unsorted list of ints'''
+
+
 def find_peak(list_of_integers):
     if not list_of_integers:
         return
@@ -8,11 +10,12 @@ def find_peak(list_of_integers):
     end = len(list_of_integers) - 1
     return(peak(list_of_integers, 0, end))
 
+
 def peak(list_int, low, high):
 
     if low == high:
-           return(list_int[high])
+        return(list_int[high])
     mid = (high + low) // 2
     if list_int[mid] > list_int[mid + 1]:
-           return(peak(list_int, low, mid))
+        return(peak(list_int, low, mid))
     return(peak(list_int, mid + 1, high))
