e3ebceecf870bc3d01ff3f285c76c4b6affbd64a
gjdame <gregoryjdame@gmail.com>
2018-07-31 18:25:27 +0000

    add two variables to begin and end for 6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -3,4 +3,15 @@
 def find_peak(list_of_integers):
     if not list_of_integers:
         return
-    return(max(list_of_integers))
+
+    beg = 0
+    end = len(list_of_integers) - 1
+
+    while (beg < end):
+        if list_of_integers[beg] > list_of_integers[end]:
+            peak = list_of_integers[beg]
+            end -= 1
+        else:
+            peak = list_of_integers[end]
+            beg += 1
+    return(peak)
