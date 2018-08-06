1f1668a2431954d370db42023a673911f0ab5c26
gjdame <gregoryjdame@gmail.com>
2018-08-06 17:53:22 +0000

    add #3

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+'''displays body of response after sending POST request with email'''
+import urllib.request
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    try:
+        req = urllib.request.Request(url)
+        with urllib.request.urlopen(req) as response:
+            html = response.read().decode('utf-8')
+            print(html)
+    except urllib.error.HTTPError as e:
+        code = e.code
+        print("Error code: {:d}".format(code))
