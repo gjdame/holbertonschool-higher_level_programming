5cff3c56067892f6a6a3b3a6d47491aff6d12fe7
gjdame <gregoryjdame@gmail.com>
2018-08-06 20:32:48 +0000

    add #5

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''fetches status of https://intranet.hbtn.io/status'''
+import requests
+import sys
+
+if __name__ == "__main__":
+
+    r = requests.get(sys.argv[1])
+    print(r.headers['X-Request-Id'])
