8bcbbd02cea40877141615e10e4a69409f8014b0
gjdame <gregoryjdame@gmail.com>
2018-08-07 00:33:04 +0000

    add 102

---
+++ 0x11-python-network_1/102-starwars.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+'''fetches status of https://intranet.hbtn.io/status'''
+import requests
+import sys
+
+if __name__ == "__main__":
+
+    param = sys.argv[1]
+    req = 'https://swapi.co/api/people/?search=' + param
+
+    r = requests.get(req)
+
+    data = r.json()
+    count = data.get('count')
+
+    print("Number of results: {}".format(count))
+
+    while (1):
+        results = data.get('results')
+        for dic in results:
+            name = dic.get('name')
+            films = dic.get('films')
+            print(name)
+            for d in films:
+                x = requests.get(d)
+                f_data = x.json()
+                name = f_data.get('title')
+                print("\t{}".format(name))
+        req = data.get('next')
+        if req is None:
+            break
+        r = requests.get(req)
+        data = r.json()
