4baf85452c4aa24a1aa212686061298aa48e20b3
gjdame <gregoryjdame@gmail.com>
2018-08-28 20:51:31 +0000

    add 103

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -4,8 +4,8 @@ let myObject = {
   value: 12
 };
 console.log(myObject);
-myObject.incr = function incr() {
-  myObject.value = myObject.value + 1;
+myObject.incr = function incr () {
+  this.value += 1;
 };
 myObject.incr();
 console.log(myObject);
