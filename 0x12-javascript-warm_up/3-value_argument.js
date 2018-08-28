7515d3d943a70fa0095c4d3ea78d00eef6948913
gjdame <gregoryjdame@gmail.com>
2018-08-28 20:18:43 +0000

    update no argument error

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -3,7 +3,7 @@
 prints statements about c, python, and javascript */
 'use strict';
 if ((process.argv[2]) === undefined) {
-  console.log('No Argument');
+  console.log('No argument');
 } else {
   console.log(process.argv[2]);
 }
