a9eace2f1812bb93f3ccdb8c11fcc67977b49ddd
gjdame <gregoryjdame@gmail.com>
2018-09-05 18:17:51 +0000

    add #4

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,28 @@
+#!/usr/bin/node
+/*
+prints the title of a Star Wars movie
+the episode number matches a given integer.
+*/
+const request = require('request');
+
+let id = '18';
+let url = 'http://swapi.co/api/films/';
+let count = 0;
+
+request(url, function (err, res, body) {
+  if (err) {
+    console.log(err);
+  } else {
+    let json = JSON.parse(body);
+    let results = json.results;
+    for (let i = 0; i < results.length; i++) {
+      for (let j = 0; j < results[i]['characters'].length; j++) {
+        let s = results[i]['characters'][j];
+        if (s.includes('people/' + id)) {
+          count += 1;
+        }
+      }
+    }
+  }
+  console.log(count);
+});
