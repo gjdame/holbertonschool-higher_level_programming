6e1a313af7290bf7437f4c843068382410e23bdf
gjdame <gregoryjdame@gmail.com>
2018-09-05 20:12:24 +0000

    add 101

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -7,22 +7,26 @@ const request = require('request');
 
 let url = 'http://swapi.co/api/films/' + process.argv[2];
 
-request(url, function (err, res, body) {
+request.get(url, function (err, res, body) {
   if (err) {
     console.log(err);
   } else {
     let results = JSON.parse(body);
     let characters = results['characters'];
+    let promises = [];
     for (let j = 0; j < characters.length; j++) {
-      let peopleUrl = characters[j];
-      request.get(peopleUrl, function (err, res, body) {
-        if (err) {
-          console.log(err);
-        } else {
-          let person = JSON.parse(body);
-          console.log(person.name);
-        }
-      });
+      promises.push(new Promise((resolve, reject) => {
+        let peopleUrl = characters[j];
+        request.get(peopleUrl, function (err, res, body) {
+          if (err) {
+            reject(err);
+          } else {
+            let person = JSON.parse(body);
+            resolve(person.name);
+          }
+        });
+      }));
     }
+    Promise.all(promises).then((names) => names.forEach((name) => console.log(name)));
   }
 });
